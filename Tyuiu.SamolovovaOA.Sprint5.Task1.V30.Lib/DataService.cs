ae96961e47c9bae225a31d74967f14e8928a42f3
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-11-21 21:29:20 +0500

    Task1

--- Tyuiu.SamolovovaOA.Sprint5.Task1.V30.Lib/DataService.cs
+++ Tyuiu.SamolovovaOA.Sprint5.Task1.V30.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.SamolovovaOA.Sprint5.Task1.V30.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\\OutPutFileTask1.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
 
             FileInfo fileInfo = new FileInfo(path);
             if (fileInfo.Exists)

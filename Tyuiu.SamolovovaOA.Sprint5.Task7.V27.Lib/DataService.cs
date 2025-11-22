464cad0ed9dca2a863ab7a6fee02278ebef1873c
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-11-22 21:34:04 +0500

    Task7

--- Tyuiu.SamolovovaOA.Sprint5.Task7.V27.Lib/DataService.cs
+++ Tyuiu.SamolovovaOA.Sprint5.Task7.V27.Lib/DataService.cs
@@ -6,44 +6,56 @@ namespace Tyuiu.SamolovovaOA.Sprint5.Task7.V27.Lib
     {
         public string LoadDataAndSave(string path)
         {
+            // 1. Папка, где лежит входной файл
+            string dir = Path.GetDirectoryName(path);
 
-            string pathSaveFile = $"{Directory.GetCurrentDirectory()}\\OutPutDataFileTask7V27.txt";
+            // 2. Полный путь к выходному файлу в той же папке
+            string pathSaveFile = Path.Combine(dir, "OutPutDataFileTask7V27.txt");
 
-            if (File.Exists(pathSaveFile))
+            // 3. Если такой файл уже есть – удаляем
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            if (fileInfo.Exists)
             {
                 File.Delete(pathSaveFile);
             }
 
-            string result = "";
+            string strLine = "";
 
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
+
+                // читаем файл построчно
                 while ((line = reader.ReadLine()) != null)
                 {
+                    strLine = "";
                     bool prevSpace = false;
 
-                    foreach (char ch in line)
+                    for (int i = 0; i < line.Length; i++)
                     {
+                        char ch = line[i];
+
                         if (ch == ' ')
                         {
                             if (!prevSpace)
                             {
-                                result += ch;
+                                strLine += ch;
                                 prevSpace = true;
                             }
                         }
                         else
                         {
-                            result += ch;
+                            strLine += ch;
                             prevSpace = false;
                         }
                     }
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
                 }
             }
-            File.WriteAllText(pathSaveFile, result);
 
-            return result;
+            // 4. По шаблону: возвращаем путь к выходному файлу
+            return pathSaveFile;
         }
     }
 }
